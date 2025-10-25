fc52afc653bad24586f7b6af49101b6c64dc1f09
raheensheikh <test@Areez-macbook.local>
2025-10-25 21:33:13 +0500

    -

--- src/components/FrozenBanner.jsx
+++ src/components/FrozenBanner.jsx
@@ -25,85 +25,51 @@ const FrozenBanner = () => {
 
       <Swiper className="banner-swiper" spaceBetween={50} slidesPerView={1}>
         <SwiperSlide className="banner-slide slide-3">
-          <Row>
-            {/* ❄️ Left Logo Animation */}
-            <Col lg={6} md={6}>
-              <motion.img
-                src={images.frozenLogo}
-                alt="Frozen Logo"
-                className="frozenLogo"
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{
-                  duration: 1.5,
-                  ease: [0.25, 0.1, 0.25, 1],
-                  type: "spring",
-                  stiffness: 90,
-                }}
-              />
-            </Col>
+          {/* ❄️ Left Logo Animation */}
+          <motion.img
+            src={images.frozenLogo}
+            alt="Frozen Logo"
+            className="frozenLogo"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 1.5,
+              ease: [0.25, 0.1, 0.25, 1],
+              type: "spring",
+              stiffness: 90,
+            }}
+          />
 
-            {/* 🧊 Right Product Animation */}
-            <Col lg={6} md={6} className="position-relative frozenWrapper">
-              {/* Box */}
-              <motion.img
-                src={images.frozenImg}
-                alt="Frozen Box"
-                className="frozenBox"
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, scale:1.1, y: -300, x: 200 }}
-                transition={{
-                  duration: 0.8,
-                  ease: [0.45, 0, 0.55, 1],
-                }}
-              />
-
-
-              {/* Hat - drop from top */}
-              <motion.img
-                src={images.frozenImg2}
-                alt="Frozen Hat"
-                className="frozenHat"
-                initial={{
-                  opacity: 0,
-                  y: -300,
-                  rotate: -20,
-                  scale: 0.8,
-                }}
-                animate={{
-                  opacity: 1,
-                  y: -480,
-                  x: -100,
-                  rotate: 15,
-                  scale: 0.4,
-                }}
-                transition={{
-                  delay: 1.5,
-                  duration: 2,
-                  type: "spring",
-                  stiffness: 120,
-                  damping: 14,
-                }}
-              />
-
-              {/* Device - slides in from right */}
-              <motion.img
-                src={images.frozenImg3}
-                alt="Frozen Device"
-                className="frozenDevice3"
-                initial={{ opacity: 0, x: 180, y: 40, scale: 0.9 }}
-                animate={{ opacity: 1, x: 200, y: 100, scale: 0.5 }}
-                transition={{
-                  delay: 1.3,
-                  duration: 1.4,
-                  type: "spring",
-                  stiffness: 100,
-                  damping: 16,
-                }}
-              />
-
-            </Col>
-          </Row>
+          {/* 🧊 Right Product Animation */}
+          <div className="position-relative frozenWrapper">
+            {/* Box */}
+            <motion.img
+              src={images.frozenImg}
+              alt="Frozen Box"
+              className="frozenBox"
+              initial={{ opacity: 0, y: 0 }}
+              animate={{ opacity: 1, scale: 1.1, y: -50, x: 100 }}
+              transition={{
+                duration: 0.8,
+                ease: [0.45, 0, 0.55, 1],
+              }}
+            />
+            {/* Device - slides in from right */}
+            <motion.img
+              src={images.frozenImg3}
+              alt="Frozen Device"
+              className="frozenDevice3"
+              initial={{ opacity: 0, x: 180, y: 40, scale: 0.9 }}
+              animate={{ opacity: 1, scale: 0.5, y: 150, x: 500 }}
+              transition={{
+                delay: 1.3,
+                duration: 1.4,
+                type: "spring",
+                stiffness: 100,
+                damping: 16,
+              }}
+            />
+          </div>
         </SwiperSlide>
       </Swiper>
     </section>
